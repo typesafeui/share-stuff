845bd3ce860651de47b8364a88f4ea712922375f
typesafeui <typesafeui@gmail.com>
2024-04-06 19:09:58 +0200

    feat: edge test

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
-import Image from "next/image";
-import fs from "fs";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export const runtime = "edge";
+
+export default async function Home() {
   async function handleSubmit(formData: FormData) {
     "use server";
     const env = formData.get("env") as string;
-    fs.writeFileSync(".share.env", env);
+    console.log("server action env", env);
   }
+  const Cookies = cookies();
+
+  const jstr = JSON.stringify(Cookies.getAll());
 
   return (
     <main className="p-12">
       <h1 className="text-4xl text-center mb-4">Share text</h1>
+      <div className="p-4 bg-slate-800 rounded-xl border border-white/10">
+        {jstr}
+      </div>
       <form
         action={handleSubmit}
         className="flex flex-col items-center w-full h-full px-4"
